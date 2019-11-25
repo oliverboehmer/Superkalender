4e954c87827bf46c2f1a04ecbea818d6369d4068
oliverboehmer <55830055+oliverboehmer@users.noreply.github.com>
2019-11-25 10:12:07 +0100

    Test back

--- superkalenderWeb/login.js
+++ superkalenderWeb/login.js
@@ -1,1 +1,12 @@
 
+
+
+    <script>
+    var modal = document.getElementById('id01');
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+    </script>
